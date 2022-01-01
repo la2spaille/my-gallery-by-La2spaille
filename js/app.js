b9d37572043a983f8bec122614837aa62b881df3
Wilfried <la2spaille@gmail.com>
2022-01-01 16:40:50 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,23 +3,8 @@ window.addEventListener('load', (e) => {
     loader.classList.add('load')
     e.stopPropagation()
 })
-const ratio = .01
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: ratio
-}
-const handleIntersect = function (entries, observer) {
-    entries.forEach(function(entry){
-        if (entry.intersectionRatio > ratio) {
-            entry.target.classList.add('is-active')
-            observer.unobserve(entry.target)
-        }
-    })
-}
-
-const observer = new IntersectionObserver(handleIntersect,options)
-window.addEventListener('load', () => {
-        observer.observe(document.querySelector('.w-gallery'))
+window.addEventListener('load', (e) => {
+       document.querySelector('.w-gallery').classList.add('is-active')
+       e.stopPropagation
   
 })
